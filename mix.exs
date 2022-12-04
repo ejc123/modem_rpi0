da50a3e4d77c0d11507501bd9c77a46ae16b7eb5
Eric J. Christeson <Eric.J.Christeson@gmail.com>
2022-12-04 16:40:25 -0600

    Modifications for modem

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi0.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi0
+  @github_organization "ejc123"
+  @app :modem_rpi0
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
